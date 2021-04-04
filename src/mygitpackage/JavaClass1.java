cd40a164f874042d6d07575faa1ccfb81ba54ef3
Suleyman63 <skoc9532@gmail.com>
2021-04-04 19:34:58 +0200

    ilk mesaj

--- src/mygitpackage/JavaClass1.java
+++ src/mygitpackage/JavaClass1.java
@@ -5,7 +5,10 @@ public class JavaClass1 {
 	public static void main(String[] args) {
 		
 System.out.println("Version 1");
+//               /Users/apple/eclipse-workspace/2021turkisch/Mygitproject
 
+
+System.out.println("version 2");
 	}
 
 }
