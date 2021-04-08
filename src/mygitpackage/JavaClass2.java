07ccd2ac0621737237e19316fc32aa515c4f3411
Suleyman63 <81867110+Suleyman63@users.noreply.github.com>
2021-04-08 20:42:04 +0200

    Update JavaClass2.java

--- src/mygitpackage/JavaClass2.java
+++ src/mygitpackage/JavaClass2.java
@@ -4,15 +4,7 @@ public class JavaClass2 {
 
 	public static void main(String[] args) {
 		
-/*
-		String isim="Kenan";
-		
-		for(int i=isim.length()-1; i>=0; i--) {
 			
-			System.out.print(isim.charAt(i));
-			
-			
-*/			
 			StringBuilder str=new StringBuilder("Kenan");
 			str.reverse();
 			
