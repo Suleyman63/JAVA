50490ae57b9587e2a0790469d4913ee984ee3398
Suleyman63 <skoc9532@gmail.com>
2021-04-10 18:35:59 +0200

    msjjj

--- src/mygitpackage/JavaClass1.java
+++ src/mygitpackage/JavaClass1.java
@@ -3,14 +3,12 @@ package mygitpackage;
 public class JavaClass1 {
 
 	public static void main(String[] args) {
-		
-System.out.println("Version 1");
+
+		System.out.println("Version 1");
 
 		System.out.println("github dan merhaba");
 
-
-System.out.println("version 2");
+		System.out.println("version 2");
 	}
 
 }
-        
