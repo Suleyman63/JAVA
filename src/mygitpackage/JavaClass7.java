235f41c750cbc177bcd0058556a01530f289fd45
Suleyman63 <skoc9532@gmail.com>
2021-04-10 20:45:47 +0200

    mmmm

--- src/mygitpackage/JavaClass7.java
+++ src/mygitpackage/JavaClass7.java
@@ -4,7 +4,6 @@ public class JavaClass7 {
 
 	public static void main(String[] args) {
 
-<<<<<<< HEAD
 		char harf;
 
 		for (harf = 'A'; harf <= 'Z'; harf++) {
@@ -12,20 +11,13 @@ public class JavaClass7 {
 			System.out.print(harf);
 		}
 
-	System.out.println();
-	
-		for (int i=1; i<=9; i++) {
-			
+		System.out.println();
+
+		for (int i = 1; i <= 9; i++) {
+
 			System.out.print(i);
 		}
-=======
 
-System.out.println("================");
-
-System.out.println("***********");
-
-
->>>>>>> 9a11ad0e31ea9baf9c0ac79d58db556ae417dd19
 	}
 
 }
