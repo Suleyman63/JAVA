414d3035d6b8bce1c3d59cd01bf514901bc93efe
Suleyman63 <skoc9532@gmail.com>
2021-04-15 18:13:26 +0200

    mj

---
+++ src/mygitpackage/JavaClass9.java
@@ -0,0 +1,22 @@
+package mygitpackage;
+
+public class JavaClass9 {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		int sayi=12345;
+		int tersi=0;
+		
+		for(; sayi !=0; sayi/=10) {
+		
+			int basamak = sayi%10;
+			tersi= tersi*10 + basamak;
+		}
+		
+		System.out.println(tersi);
+		
+	}
+
+}
