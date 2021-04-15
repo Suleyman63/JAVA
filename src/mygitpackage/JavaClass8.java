145067fd78b03cf9ac519be743d38bfd2c424cfb
Suleyman63 <skoc9532@gmail.com>
2021-04-15 18:01:30 +0200

    mmssjjj

---
+++ src/mygitpackage/JavaClass8.java
@@ -0,0 +1,20 @@
+package mygitpackage;
+
+public class JavaClass8 {
+
+	public static void main(String[] args) {
+		
+
+		int taban=3;
+		int kuvvet=4;
+		
+		int sonuc=1;
+		
+		
+		for(;kuvvet>0; --kuvvet) {
+			sonuc*=taban;
+		}
+		System.out.print(sonuc);
+	}
+
+}
