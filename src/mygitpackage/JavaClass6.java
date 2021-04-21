30ca2f8ff99e3493e6bc142fe926871909171538
Suleyman63 <skoc9532@gmail.com>
2021-04-21 22:24:47 +0200

    msj

--- src/mygitpackage/JavaClass6.java
+++ src/mygitpackage/JavaClass6.java
@@ -1,14 +1,24 @@
 package mygitpackage;
 
+import java.util.Arrays;
+
 public class JavaClass6 {
 
 	public static void main(String[] args) {
 		
 
-		System.out.println("Hello");
+		int arr[]= {2,3,4,6,8,9};
 		
-		System.out.println("kenan");
+		int temp=arr[0];
 		
+		for(int i=0; i<arr.length-1; i++) {
+			
+			arr[0]=arr[1];
+		}
+		
+		arr[arr.length-1]=temp;
+		
+		System.out.println(Arrays.toString(arr));
 	}
 
 }
