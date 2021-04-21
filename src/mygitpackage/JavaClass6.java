7c5d9880912c2f3929fbd61c545d2ec649fe64e7
Suleyman63 <skoc9532@gmail.com>
2021-04-21 22:27:24 +0200

    mss

--- src/mygitpackage/JavaClass6.java
+++ src/mygitpackage/JavaClass6.java
@@ -5,19 +5,18 @@ import java.util.Arrays;
 public class JavaClass6 {
 
 	public static void main(String[] args) {
-		
 
-		int arr[]= {2,3,4,6,8,9};
-		
-		int temp=arr[0];
-		
-		for(int i=0; i<arr.length-1; i++) {
-			
-			arr[0]=arr[1];
+		int arr[] = { 2, 3, 4, 6, 8, 9 };
+
+		int temp = arr[0];
+
+		for (int i = 0; i < arr.length - 1; i++) {
+
+			arr[i] = arr[i+1];
 		}
-		
-		arr[arr.length-1]=temp;
-		
+
+		arr[arr.length - 1] = temp;
+
 		System.out.println(Arrays.toString(arr));
 	}
 
